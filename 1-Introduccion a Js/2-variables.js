d3d49bf9676af21f8048ce368a2868fb93261a01
Alejandro Gonzalez <alex1123.nel@gmail.com>
2025-05-08 19:59:20 -0600

    2-Variables

---
+++ 1-Introduccion a Js/2-variables.js
@@ -0,0 +1,29 @@
+
+// Operaciones
+var variable1 = 2
+var variable2 = variable1
+
+var suma = variable1 + variable2
+var resta = variable1 - variable2
+var multiplicacion = variable1 * variable2
+var division = variable1 / variable2
+
+alert(division)
+
+// Auto operaciones
+var variable3 = 2
+variable3 += 1
+variable3 ++ //variable3 += 1
+variable3 -= 1
+variable3 *= 2
+variable3 /= 2
+
+alert(variable3)
+
+// Residuo
+var variable4 = 2
+var variable5 = 5
+
+var modulo = variable5 % variable4
+
+alert(modulo)
