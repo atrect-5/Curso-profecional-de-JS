812d3cb378c239ac94de68fac41a9390438499e5
Alejandro Gonzalez <alex1123.nel@gmail.com>
2025-05-08 21:51:14 -0600

    7-bucles do while

---
+++ 1-Introduccion a Js/7-bucle do while.js
@@ -0,0 +1,8 @@
+
+var variable = 0
+
+do{ // Se ejecuta una vez antes de evaluar
+    variable++
+}while(variable <= 10)
+
+alert(`La variable vale ${variable}`) // variable = 11, porque se eecuta 1 vez mas antes de evaluar
