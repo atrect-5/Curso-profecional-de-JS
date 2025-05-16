8643d617bdb14e2d02b6d7c735d121c18e4575a2
Alejandro Gonzalez <alex1123.nel@gmail.com>
2025-05-15 22:25:23 -0600

    5.1-AddEventListener

---
+++ 5-Eventos/1-AddEventListener.js
@@ -0,0 +1,13 @@
+
+// Se asigna por JS una funcion al evento onClick al boton
+const agregaEvento = () => {
+    const elemento = document.querySelector('button')
+    elemento.addEventListener('click', mostrarMensaje)
+}
+
+const mostrarMensaje = () => {
+    alert('Se presiono del boton')
+}
+
+// Se asegura que al cargarse la pagina se le asigne el evento al onClock del boton
+addEventListener('load', agregaEvento)
