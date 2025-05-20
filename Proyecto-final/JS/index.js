5194bd502046f3b4ab5e5b94b30b3a7493d3df4e
Alejandro Gonzalez <alex1123.nel@gmail.com>
2025-05-19 22:25:55 -0600

    writing on inventory table with stored data

--- Proyecto-final/JS/index.js
+++ Proyecto-final/JS/index.js
@@ -1,6 +1,7 @@
 
-
+// Declaramos los objetos con los valores iniciales 
 let cash = _cashier(db_cash)
+let products = _products(db_products)
 
 // Muestra en el navegador la cantidad de dinero disponible en caja
 const writeCash = () => {
@@ -8,7 +9,27 @@ const writeCash = () => {
     cashier.innerHTML = `Caja: $ ${cash.getStored()} MXN`
 }
 
+// Muestra en el navegador los productos que hay disponibles
+const writeInventory = () => {
+    let inventoryTable = document.getElementById('inventory-table')
+    let bodyInventoryTable = inventoryTable.getElementsByTagName('tbody')[0]
+    bodyInventoryTable.innerHTML = ''
+    
+    products.getAll().forEach(product => {
+        let row = document.createElement('tr')
+        row.innerHTML = `
+        <td>${product.id}</td>
+        <td>${product.name}</td>
+        <td>${product.stored}</td>
+        <td>${product.price}</td>
+        `
+        bodyInventoryTable.appendChild(row)
+    })
+}
 
 
 // Carga la funcion writeCash cuando carga la pagina
 addEventListener('load', writeCash)
+
+// Carga la funcion writeInventory cuando carga la pagina
+addEventListener('load', writeInventory)
